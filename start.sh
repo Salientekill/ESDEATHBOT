d1f4d2108524d5d9e84d9a53097234be1f215d8c
Your Name <you@example.com>
2024-06-15 12:32:23 -0300

    LOTUSDEV

--- start.sh
+++ start.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+#!bin/bash
 
 if [[ "$*" == *"-code"* ]]; then
-  if [ ! -f DADOS/ESDEATH-QR/creds.json ]; then
-    echo -e "\e[32mO arquivo creds.json não foi encontrado então será iniciado com código de emparelhamento.\e[0m"
-    while : 
-    do
-      echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
-      node --trace-warnings iniciar.js -code 
-      sleep 1
-    done
-  else
-    echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
-    node --trace-warnings iniciar.js 
+if [ ! -f DADOS/ESDEATH-QR/creds.json ]; then
+echo -e "\e[32mO arquivo creds.json não foi encontrado então será iniciado com código de emparelhamento.\e[0m"
+while : 
+do
+echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
+node --trace-warnings iniciar.js -code 
+sleep 1
+done
+else
+echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
+node --trace-warnings iniciar.js 
   fi
 else
-  while : 
-  do
-    echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
-    node --trace-warnings iniciar.js
-    sleep 1
-  done
+while : 
+do
+echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE..\e[0m"
+NODE_NO_WARNINGS=1 node --trace-warnings iniciar.js
+sleep 1
+done
 fi
