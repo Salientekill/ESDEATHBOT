ddc64612e6eb088e0494b980c8aea5b9e669586c
Lotus / <default_email@example.com>
2024-12-13 15:54:40 +0000

    Big Fix

--- start.sh
+++ start.sh
@@ -5,16 +5,38 @@ cleanup_files() {
 }
 
 start_node_script() {
-              echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE...\e[0m"
+    echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE...\e[0m"
     NODE_NO_WARNINGS=1 node --trace-deprecation iniciar.js
 }
 
 start_node_script_with_code() {
-              echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE...\e[0m"
+    echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE...\e[0m"
     NODE_NO_WARNINGS=1 node --trace-deprecation iniciar.js -code
 }
 
-if echo "$*" | grep -q -- "-code"; then
+start_node_script_with_code2() {
+    echo -e "\e[32mESDEATH BOT ESTÁ INICIANDO AGUARDE...\e[0m"
+    NODE_NO_WARNINGS=1 node --trace-deprecation iniciar.js -code2
+}
+
+if echo "$*" | grep -q -- "-code2"; then
+    if [ ! -f DADOS/ESDEATH-QR/creds.json ]; then
+        echo -e "\e[32m - iniciado com código de emparelhamento...\e[0m"
+        while : 
+        do
+            cleanup_files
+            start_node_script_with_code2
+            sleep 1
+        done
+    else
+        while : 
+        do
+            cleanup_files
+            start_node_script
+            sleep 1
+        done
+    fi
+elif echo "$*" | grep -q -- "-code"; then
     if [ ! -f DADOS/ESDEATH-QR/creds.json ]; then
         echo -e "\e[32m - iniciado com código de emparelhamento...\e[0m"
         while : 
