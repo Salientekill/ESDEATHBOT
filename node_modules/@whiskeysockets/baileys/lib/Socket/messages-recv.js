037f9b0a548584a2e97d14a4a1100d27acb3e4ec
Lotus dev <lotus@example.com>
2025-01-31 20:36:59 +0000

    V10

--- node_modules/@whiskeysockets/baileys/lib/Socket/messages-recv.js
+++ node_modules/@whiskeysockets/baileys/lib/Socket/messages-recv.js
@@ -774,19 +774,19 @@ const makeMessagesRecvSocket = (config) => {
     };
     const handleBadAck = async ({ attrs }) => {
         const key = { remoteJid: attrs.from, fromMe: true, id: attrs.id };
-        // current hypothesis is that if pash is sent in the ack
-        // it means -- the message hasn't reached all devices yet
-        // we'll retry sending the message here
-        if (attrs.phash) {
-            logger.info({ attrs }, 'received phash in ack, resending message...');
-            const msg = await getMessage(key);
-            if (msg) {
-                await relayMessage(key.remoteJid, msg, { messageId: key.id, useUserDevicesCache: false });
-            }
-            else {
-                logger.warn({ attrs }, 'could not send message again, as it was not found');
-            }
-        }
+        // WARNING: REFRAIN FROM ENABLING THIS FOR NOW. IT WILL CAUSE A LOOP
+        // // current hypothesis is that if pash is sent in the ack
+        // // it means -- the message hasn't reached all devices yet
+        // // we'll retry sending the message here
+        // if(attrs.phash) {
+        // 	logger.info({ attrs }, 'received phash in ack, resending message...')
+        // 	const msg = await getMessage(key)
+        // 	if(msg) {
+        // 		await relayMessage(key.remoteJid!, msg, { messageId: key.id!, useUserDevicesCache: false })
+        // 	} else {
+        // 		logger.warn({ attrs }, 'could not send message again, as it was not found')
+        // 	}
+        // }
         // error in acknowledgement,
         // device could not display the message
         if (attrs.error) {
